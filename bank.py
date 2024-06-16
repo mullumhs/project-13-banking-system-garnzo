a9934e4c76188cb87d139c17b49b0fe8a2c5c2bd
Garnzo <92022699+garnzo@users.noreply.github.com>
2024-06-17 09:48:54 +1000

    still trying to figure out the transfer method

--- bank.py
+++ bank.py
@@ -4,3 +4,27 @@
 # Author: 
 # Date: 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+from account import Account
+
+class Bank:
+
+    def __init__(self, name, bal, create_acc):
+        self._accounts = []
+    
+    def create_acc(self, name, bal):
+        for account in self._accounts:
+            if name == name.get_acc_info():
+                raise ValueError("Account already exists")
+        self._accounts.append(name)
+
+    def del_acc(self, name):
+        for account in self._accounts:
+            if name != name.get_acc_info():
+                raise ValueError("Account doesn't exist")
+        self._accounts.remove(account)
+
+    def transfer(self, name, bal):
+        for account in self._accounts:
+            if name != name.get_acc_info():
+                raise ValueError("Transaction Canceled, Account Doesn't Exist")
+        
