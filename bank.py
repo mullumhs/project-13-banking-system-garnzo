5fe5e282a8643c6ecbce807eff463eab391b615b
Garnzo <92022699+garnzo@users.noreply.github.com>
2024-06-18 11:38:07 +1000

    my brain hurts

--- bank.py
+++ bank.py
@@ -24,7 +24,18 @@ class Bank:
         self._accounts.remove(account)
 
     def transfer(self, name, bal):
-        for account in self._accounts:
+        for account1 in self._accounts:
             if name != name.get_acc_info():
                 raise ValueError("Transaction Canceled, Account Doesn't Exist")
-        
+        for account2 in self._accounts:
+            if name != name.get_acc_info():
+                raise ValueError("Transaction Canceled, Account Doesn't Exist")
+         
+    def depo(self, name, bal):
+        for account in self._accounts:
+            if name != name.get_acc_info(account):
+                raise ValueError("Account doesn't Exist")
+        self.depo()
+                
+    def withdraw():
+        pass
